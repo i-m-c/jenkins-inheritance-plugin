943cef778afa2ebd1eccdda9c77bfe865b489a7c
Adam Fitzgerald <Adam.Fitzgerald@morganstanley.com>
2014-07-03 11:39:43 -0400

    add missing copyright notices

--- src/main/java/hudson/plugins/project_inheritance/projects/inheritance/PromotedBuildInheritanceChecker.java
+++ src/main/java/hudson/plugins/project_inheritance/projects/inheritance/PromotedBuildInheritanceChecker.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright © 2014 Contributor.  All rights reserved.
+ * 
+ * This file is part of the Inheritance plug-in for Jenkins.
+ * 
+ * The Inheritance plug-in is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation in version 3
+ * of the License
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package hudson.plugins.project_inheritance.projects.inheritance;
 
 import hudson.Extension;
