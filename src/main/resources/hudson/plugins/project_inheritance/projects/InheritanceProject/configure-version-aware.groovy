48ea4b1e1fc33a52f757e2b3d69682f986c71946
Martin Schroeder <martin@travelog-app.de>
2021-04-19 19:18:43 +0200

    Adjusted inheritance config to correspond to Jenkins look
    This also fixes div-flow issues with some plugins after the Jenkins
    2.277 table->div upgrade

--- src/main/resources/hudson/plugins/project_inheritance/projects/InheritanceProject/configure-version-aware.groovy
+++ src/main/resources/hudson/plugins/project_inheritance/projects/InheritanceProject/configure-version-aware.groovy
@@ -31,99 +31,116 @@ st = namespace("jelly:stapler")
 adjunctPrefix = "hudson.plugins.project_inheritance.projects.InheritanceProject.adjunct"
 
 
-l.layout(title: my.getDisplayName() + " Config", permission: my.EXTENDED_READ, norefresh: "true") {
+l.layout(
+		//type: "one-column",
+		title: _("Config", my.getDisplayName()),
+		permission: my.EXTENDED_READ,
+		norefresh: "true"
+) {
 	include(my, "sidepanel.jelly")
+	
+	l.js(src: "jsbundles/config-scrollspy.js")
+	l.css(src: "jsbundles/config-scrollspy.css")
+	
 	
 	//Initialize the breadcrumps for the config-sections
 	f.breadcrumb_config_outline()
 	
 	//Render the main panel
 	l.main_panel() {
-		//Show a scrim while loading
-		div(class: "behavior-loading", _("LOADING"))
-		
-		//Add an invisible div element, that stores which validationErrors can be safely ignored
-		div(
-				style: "display:none",
-				class: "acceptable-error-url-data",
-				ProjectCreationEngine.instance.getAcceptableErrorUrls()
-		)
-		
-		//Load additional Javascript
-		st.adjunct(includes: adjunctPrefix + ".detectValidationErrors")
-		st.adjunct(includes: adjunctPrefix + ".confirmChangesPopup")
-		//Prompt the user to enter a version message
-		if (my.getCurrentVersionNotification().areAllVersionsUnstable()) {
-			buttonMsg = _("Warning! Your changes will get effective immediately if you press OK.\\nPlease type in short description of what you changed:")
-		} else {
-			buttonMsg = _("Please type in short description of what you changed:")
-		}
-		saveButtonFunction = "return confirmChangesAndEnterVersion(this, '" + buttonMsg + "')"
-		
-		//Now, add the actual form (copied from the default config.jelly of "Job")
-		ct.form(
-				name: "config", action: "configSubmit", method: "post",
-				onsubmit: saveButtonFunction
-		) {
-			descriptor = my.getDescriptor()
-			instance = my
-			
-			//Show elements that should display in the page header (especially warnings)
-			include(my, "configure-header-warnings")
-			
-			if (my.isNameEditable()) {
-				if (my.pronoun != null && !my.pronoun.isEmpty()) {
-					f.entry(title: my.getPronoun() + " " + _("name")) {
-						f.textbox(name: "name", value: my.name)
-					}
-				} else {
-					f.entry(title: _("Project name")) {
-						f.textbox(name: "name", value: my.name)
-					}
-				}
-			}
-			
-			/* The app markup formatter may or may not have the "codeMirror*" methods.
-			 * In Jelly, this seems to not be a problem; but in Groovy it croaks.
-			 * As such, we must wrap the assignment try-catch
-			 */
-			try {
-				codemirrorConfig = app.markupFormatter.codeMirrorConfig;
-				codemirrorMode = app.markupFormatter.codeMirrorMode;
-			} catch (ex) {
-				codemirrorConfig = null;
-				codemirrorMode = null;
-			}
-			
-			f.entry(title: _("Description"), help: app.markupFormatter.helpUrl) {
-				f.textarea(
-						"codemirror-config": codemirrorConfig,
-						"codemirror-mode": codemirrorMode,
-						name: "description",
-						value: my.description,
-						previewEndpoint: "/markupFormatter/previewDescription"
+		div(class: "container") { div(class: "row") {
+			div(class:"col-md-offset-2 col-md-30") {
+				//Show a scrim while loading
+				div(class: "behavior-loading", _("LOADING"))
+				
+				//Add an invisible div element, that stores which validationErrors can be safely ignored
+				div(
+					style: "display:none",
+					class: "acceptable-error-url-data",
+					ProjectCreationEngine.instance.getAcceptableErrorUrls()
 				)
-			}
-			
-			ct.colored_block(backCol: "LightGoldenRodYellow ", borderCol: "navy") {
-				f.section(title: _("Properties")) {
-					f.descriptorList(
-							field: "properties",
-							forceRowSet: "true",
-							descriptors: my.getJobPropertyDescriptors(my.getClass(), true)
-					)
+				
+				//Load additional Javascript
+				st.adjunct(includes: adjunctPrefix + ".detectValidationErrors")
+				st.adjunct(includes: adjunctPrefix + ".confirmChangesPopup")
+				
+				//Prompt the user to enter a version message
+				if (my.getCurrentVersionNotification().areAllVersionsUnstable()) {
+					buttonMsg = _("Warning! Your changes will get effective immediately if you press OK.\\nPlease type in short description of what you changed:")
+				} else {
+					buttonMsg = _("Please type in short description of what you changed:")
+				}
+				saveButtonFunction = "return confirmChangesAndEnterVersion(this, '" + buttonMsg + "')"
+				
+				//Now, add the actual form (copied from the default config.jelly of "Job")
+				f.form(
+						method: "post",
+						name: "config",
+						action: "configSubmit",
+						onsubmit: saveButtonFunction,
+						tableClass: "config-table scrollspy"
+				) {
+					descriptor = my.getDescriptor()
+					instance = my
+					
+					//Show elements that should display in the page header (especially warnings)
+					include(my, "configure-header-warnings")
+					
+					if (my.isNameEditable()) {
+						if (my.pronoun != null && !my.pronoun.isEmpty()) {
+							f.entry(title: my.getPronoun() + " " + _("name")) {
+								f.textbox(name: "name", value: my.name)
+							}
+						} else {
+							f.entry(title: _("Project name")) {
+								f.textbox(name: "name", value: my.name)
+							}
+						}
+					}
+					
+					/* The app markup formatter may or may not have the "codeMirror*" methods.
+					 * In Jelly, this seems to not be a problem; but in Groovy it croaks.
+					 * As such, we must wrap the assignment try-catch
+					 */
+					try {
+						codemirrorConfig = app.markupFormatter.codeMirrorConfig;
+						codemirrorMode = app.markupFormatter.codeMirrorMode;
+					} catch (ex) {
+						codemirrorConfig = null;
+						codemirrorMode = null;
+					}
+					
+					f.entry(title: _("Description"), help: app.markupFormatter.helpUrl) {
+						f.textarea(
+								"codemirror-config": codemirrorConfig,
+								"codemirror-mode": codemirrorMode,
+								name: "description",
+								value: my.description,
+								previewEndpoint: "/markupFormatter/previewDescription"
+						)
+					}
+					
+					ct.colored_block(backCol: "LightGoldenRodYellow ", borderCol: "navy") {
+						f.section(title: _("Properties")) {
+							f.descriptorList(
+									field: "properties",
+									forceRowSet: "true",
+									descriptors: my.getJobPropertyDescriptors(my.getClass(), true)
+							)
+						}
+					}
+					
+					//Load the extended configuration
+					include(my, "configure-entries.groovy")
+					
+					//Show the save/apply buttons, if allowed
+					if (h.hasPermission(it, my.CONFIGURE)) {
+						f.bottomButtonBar() {
+							f.submit(value: _("Save"))
+						}
+					}
 				}
 			}
-			
-			//Load the extended configuration
-			include(my, "configure-entries.groovy")
-			
-			//Show the save/apply buttons, if allowed
-			if (h.hasPermission(it, my.CONFIGURE)) {
-				f.bottomButtonBar() {
-					f.submit(value: _("Save"))
-				}
-			}
-		}
+		}}
 	}
 }
